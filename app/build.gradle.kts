a315eec9d3df5415c09f24ab72b1acb200e4a6ca
FightofDestinyHD <FightofDestinyHD@users.noreply.github.com>
2026-08-25 21:31:08 +0200

    release: v0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
 		applicationId = "com.speckdealer.app"
 		minSdk = 24
 		targetSdk = 34
-		versionCode = 65
-		versionName = "0.2.0"
+		versionCode = 66
+		versionName = "0.2.1"
 		buildConfigField("boolean", "ENABLE_DEV_MODE", "true")
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
